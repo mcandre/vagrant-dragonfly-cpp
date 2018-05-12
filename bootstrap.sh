69103e8b65f2f9c3ff58a4a42a018712ecf8f4d4
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-05-12 16:03:29 -0500

    update base box

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-
 sudo pkg update &&
-    sudo pkg install -y llvm50
-
-# Remove trailing '50' from binaries
-for f in $(find /usr/local/bin -type f -name '*50' -print); do
-    f_sans50="$(echo $f | sed 's/50//')" &&
-        ln -s "$f" "$f_sans50"
-done
-
-sudo pkg clean -y
+    sudo pkg install -y llvm50 &&
+    sudo pkg clean -y &&
+    sudo sed -i -E -e 's/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/games \/usr\/local\/sbin \/usr\/local\/bin \/usr\/pkg\/sbin \/usr\/pkg\/bin ~\/bin:\\$/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/games \/usr\/local\/sbin \/usr\/local\/bin \/usr\/pkg\/sbin \/usr\/pkg\/bin ~\/bin \/usr\/local\/llvm50\/bin:\\/' /etc/login.conf &&
+    sudo cap_mkdb /etc/login.conf
