137f235443da06c874b76ec9199369eb1364c2b1
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-02-26 19:50:57 -0600

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo pkg update &&
-    sudo pkg install -y llvm50 &&
+    sudo pkg install -y llvm80 &&
     sudo pkg clean -y &&
-    sudo sed -i -E -e 's/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/games \/usr\/local\/sbin \/usr\/local\/bin \/usr\/pkg\/sbin \/usr\/pkg\/bin ~\/bin:\\$/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/games \/usr\/local\/sbin \/usr\/local\/bin \/usr\/pkg\/sbin \/usr\/pkg\/bin ~\/bin \/usr\/local\/llvm50\/bin:\\/' /etc/login.conf &&
+    sudo sed -i -E -e 's/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/games \/usr\/local\/sbin \/usr\/local\/bin \/usr\/pkg\/sbin \/usr\/pkg\/bin ~\/bin:\\$/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/games \/usr\/local\/sbin \/usr\/local\/bin \/usr\/pkg\/sbin \/usr\/pkg\/bin ~\/bin \/usr\/local\/llvm80\/bin:\\/' /etc/login.conf &&
     sudo cap_mkdb /etc/login.conf
